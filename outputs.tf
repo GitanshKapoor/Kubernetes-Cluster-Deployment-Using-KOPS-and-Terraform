345de26e5087914ffe8dc40fddba7cb8c63e5c56
Gitansh Kapoor <72307552+GitanshKapoor@users.noreply.github.com>
2025-08-06 10:24:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,10 @@ output "route53_zone_id" {
 }
 
 output "Run - kops_create_command on Kops Instance" {
-  value = "kops create cluster --name=wordpress.gitanshdevops.space --state=s3://${aws_s3_bucket.kops_state.bucket} --zones=us-east-1a,us-east-1b --node-count=2 --node-size=t3.small --control-plane-size=t3.medium --dns-zone=wordpress.gitanshdevops.space --node-volume-size=12 --control-plane-volume-size=12 --ssh-public-key=/home/ubuntu/.ssh/id_rsa.pub"
+  value = "kops create cluster --name=<example.yourdomain.com> --state=s3://${aws_s3_bucket.kops_state.bucket} --zones=us-east-1a,us-east-1b --node-count=2 --node-size=t3.small --control-plane-size=t3.medium --dns-zone=<example.yourdomain.com> --node-volume-size=12 --control-plane-volume-size=12 --ssh-public-key=/home/ubuntu/.ssh/id_rsa.pub"
 }
 
 output "kops_update_command on Kops Instance" {
-  value = "kops update cluster --name=wordpress.gitanshdevops.space --state=s3://${aws_s3_bucket.kops_state.bucket} --yes --admin"
+  value = "kops update cluster --name=<example.yourdomain.com> --state=s3://${aws_s3_bucket.kops_state.bucket} --yes --admin"
 }
+
